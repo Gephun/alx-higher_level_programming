b01efc3b22e4dab3e013fe5facd8cdb2cb4ea40f
Gephun <gephunonyango@gmail.com>
2022-10-31 23:50:57 -0700

    all tasks

---
+++ 0x0C-python-almost_a_circle/tests/test_square.py
@@ -0,0 +1,92 @@
+# #!/usr/bin/python3
+# """Unittest square.
+# Test cases for the Square class.
+# Each test has the number of the task,
+# and the number of the test for that task
+# (i.e 'test_17_0' for the first test of task 17)
+# """
+
+# import unittest
+# import io
+# import contextlib
+# from models.base import Base
+# from models.rectangle import Rectangle
+# from models.square import Square
+
+
+# class TestSquare(unittest.TestCase):
+#     """Test class for Square class."""
+
+#     def setUp(self):
+#         Base._Base__nb_objects = 0
+
+#     def test_10_0(self):
+#         """Test Square class: check for attributes."""
+
+#         s0 = Square(1)
+#         self.assertEqual(s0.id, 1)
+#         s1 = Square(5, 3, 4)
+#         self.assertEqual(s1.height, 5)
+#         self.assertEqual(s1.width, 5)
+#         self.assertEqual(s1.x, 3)
+#         self.assertEqual(s1.y, 4)
+#         self.assertEqual(s1.id, 2)
+
+#     def test_10_1(self):
+#         """Test __str__ representation."""
+
+#         s1 = Square(9, 4, 5, 6)
+#         self.assertEqual(str(s1), "[Square] (6) 4/5 - 9")
+
+#     def test_10_2(self):
+#         """Test Square class: check for inheritance."""
+
+#         s1 = Square(6)
+#         self.assertTrue(isinstance(s1, Rectangle))
+#         self.assertTrue(issubclass(Square, Rectangle))
+#         self.assertFalse(isinstance(Square, Rectangle))
+#         self.assertTrue(isinstance(s1, Base))
+#         self.assertTrue(issubclass(Square, Base))
+#         self.assertFalse(isinstance(Square, Base))
+
+#     # def test_10_3(self):
+#     #     """Test Square class: check for missing args."""
+
+#     #     with self.assertRaises(TypeError) as x:
+#     #         s1 = Square()
+#     #     self.assertEqual(
+#     #         "__init__() missing 1 required positional argument: 'size'", str(
+#     #             x.exception))
+
+#     def test_10_4(self):
+#         """Test Square for methods inherited from Rectangle."""
+
+#         s1 = Square(9)
+#         self.assertEqual(s1.area(), 81)
+#         s2 = Square(4, 1, 2, 5)
+#         s2.update(7)
+#         self.assertEqual(s2.id, 7)
+#         f = io.StringIO()
+#         s3 = Square(4)
+#         with contextlib.redirect_stdout(f):
+#             s3.display()
+#         s = f.getvalue()
+#         res = "####\n####\n####\n####\n"
+#         self.assertEqual(s, res)
+
+#     def test_11_0(self):
+#         """Test Square class: check for size attribute."""
+
+#         s1 = Square(8)
+#         self.assertEqual(s1.size, 8)
+#         s2 = Square(9, 8, 7, 2)
+#         self.assertEqual(s2.size, 9)
+
+#     def test_11_1(self):
+#         """Test Square class: check for wrong attributes."""
+
+#         with self.assertRaises(TypeError) as x:
+#             s = Square("Hello", 2)
+#         self.assertEqual("width must be an integer", str(x.exception))
+#         with self.assertRaises(TypeError) as x:
+
